3dfe6ce7e0d8c1212a3b31517e9c38b136571a04
purerosefallen <78877@qq.com>
2019-08-12 15:00:18 +0800

    fix

--- index.js
+++ index.js
@@ -109,18 +109,15 @@ bot.on("message", (data) => {
 		return;
 	}
 	switch (parsed_msg[0]) { 
-		case ":roomlist": { 
+		case ":roomlist":
 			get_roomlist(data);
 			break;
-		}
-		case ":testcard": {
+		case ":testcard":
 			test_card(data, parsed_msg);
 			break;
-		}
-		default: { 
+		default:
 			send_help(data);
 			break;
-		}
 	}
 });
 load_databases();
